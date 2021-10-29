8661c4eeb2c2f2d07c97f7ecec40dd450b3a2696
Egor Sergeev <egors20000@mail.ru>
2021-10-29 12:02:55 +0300

    3_1(full) and 4_1(part)

--- 3_1/3_1.cpp
+++ 3_1/3_1.cpp
@@ -1,22 +1,255 @@
-﻿#include <iostream>
+﻿//Реализовать очередь с динамическим зацикленным буфером.
+//Обрабатывать команды push back и pop front.
+//
+//Если дана команда pop front, то число b - ожидаемое значение.
+//Если команда pop front вызвана для пустой структуры данных, то ожидается “ - 1”.
+//
+//Требуется напечатать YES - если все ожидаемые значения совпали.
+//Иначе, если хотя бы одно ожидание не оправдалось, то напечатать NO.
+
+#include <iostream>
+#include <sstream>
+#include <assert.h>
+#define MIN_QUEUE_CAPACITY 8
+#define RESIZE_FACTOR 2
 
 
 
-class Deque
+class Queue
 {
 public:
+    Queue()
+    {
+        capacity = MIN_QUEUE_CAPACITY;
+        size = 0;
+        data = new int[capacity];
+        front = -1;
+        back = -1;
+    }
 
+    ~Queue()
+    {
+        delete[] data;
+    }
 
+    void push(int var)
+    {
+        if (full())
+        {
+            int* new_data = new int[capacity * RESIZE_FACTOR];
+            for (int i = 0; i < size; ++i)
+            {
+                new_data[front + i] = data[(front + i) % capacity];
+            }
+            back = front + size - 1;
+            capacity *= RESIZE_FACTOR;
 
+            delete[] data;
+            data = new_data;
+        }
+
+        if (empty())
+            front = 0;
+        
+        back = (back + 1) % capacity;
+        data[back] = var;
+        size++;
+    }
+
+    int pop()
+    {
+        if (empty())
+            return -1;
+
+        int tmp = data[front];
+        if (front == back) // в структуре только один элемент, теперь она пустая
+        {
+            front = -1;
+            back = -1;
+        }
+        else
+            front = (front + 1) % capacity;
+
+        size--;
+        return tmp;
+    }
+
+    void print(std::ostream& output)
+    {
+        if (!empty())
+        {
+            output << data[front];
+            for (int i = 1; i < size; ++i)
+                output << ' ' << data[(front + i) % capacity];
+        }
+    }
 
 private:
+    bool empty()
+    {
+        return (front == -1 && back == -1);
+    }
 
+    bool full()
+    {
+        return (size == capacity);
+    }
+
+
+    int* data;
+    int size, capacity, front, back;
 };
+
+void request(std::istream& in, std::ostream& out)
+{
+    int n = 0;
+    in >> n;
+
+    Queue q;
+    bool flag = true;
+
+    for (int i = 0; i < n; ++i)
+    {
+        int a = 0, b = 0;
+        in >> a >> b;
+
+        if (a == 3)
+            q.push(b);
+
+        if (a == 2)
+            if (q.pop() != b)
+                flag = false;
+    }
+
+    out << (flag ? "YES" : "NO");
+}
+
+void test_Queue()
+{
+    {
+        Queue q;
+
+        std::stringstream s;
+        q.print(s);
+
+        assert(s.str() == "");
+        assert(q.pop() == -1);
+    }
+
+    {
+        Queue q;
+        q.push(1);
+        q.push(2);
+        q.push(3);
+        q.push(4);
+        q.push(5);
+        
+        std::stringstream s;
+        q.print(s);
+
+        assert(s.str() == "1 2 3 4 5");
+    }
+
+    {
+        Queue q;
+        q.push(1);
+        q.push(2);
+        q.push(3);
+        q.push(4);
+        q.push(5);
+        q.pop();
+        q.pop();
+
+        std::stringstream s;
+        q.print(s);
+
+        assert(s.str() == "3 4 5");
+    }
+
+    {
+        Queue q;
+
+        for (int i = 0; i < 100; ++i)
+            q.push(i);
+
+        for (int i = 0; i < 100; ++i)
+            assert(q.pop() == i);
+    }
+
+    std::cout << "ALL QUEUE TESTS PASSED" << std::endl;
+}
+
+void test_request()
+{
+    {
+        std::stringstream in;
+        in << "3\n";
+        in << "3 44\n";
+        in << "3 50\n";
+        in << "2 44\n";
+
+        std::stringstream out;
+        request(in, out);
+        
+        assert(out.str() == "YES");
+    }
+
+    {
+        std::stringstream in;
+        in << "2\n";
+        in << "2 -1\n";
+        in << "3 10\n";
+
+        std::stringstream out;
+        request(in, out);
+
+        assert(out.str() == "YES");
+    }
+
+    {
+        std::stringstream in;
+        in << "2\n";
+        in << "3 44\n";
+        in << "2 66\n";
+
+        std::stringstream out;
+        request(in, out);
+
+        assert(out.str() == "NO");
+    }
+
+    {
+        std::stringstream in;
+        in << "10\n";
+
+        in << "3 1\n";
+        in << "3 2\n";
+        in << "3 3\n";
+        in << "3 4\n";
+        in << "3 5\n";
+
+        in << "2 1\n";
+        in << "2 2\n";
+        in << "2 3\n";
+        in << "2 4\n";
+        in << "2 5\n";
+
+        std::stringstream out;
+        request(in, out);
+
+        assert(out.str() == "YES");
+    }
+
+    std::cout << "ALL REQUEST TESTS PASSED" << std::endl;
+}
 
 
 int main()
 {
-    
+    test_Queue();
+    test_request();
+    //request(std::cin, std::cout);
+
 
     return 0;
 }
---
+++ 4_1/4_1.cpp
@@ -0,0 +1,174 @@
+﻿#include <iostream>
+#define MIN_CAPACITY 3
+#define INCREASE_FACTOR 2
+
+
+template <typename T>
+class Heap
+{
+public:
+    Heap()
+    {
+        arr = new T[MIN_CAPACITY];
+        capacity = MIN_CAPACITY;
+        size = 0;
+    }
+
+    Heap(T* a, int s)
+    {
+        arr = a;
+        capacity = (s > MIN_CAPACITY ? s : MIN_CAPACITY);
+        size = s;
+        build_heap();
+    }
+
+    Heap(T* a, int s, bool (*c)(const T&, const T&))
+    {
+        arr = a;
+        size = s;
+        capacity = (s > MIN_CAPACITY ? s : MIN_CAPACITY);
+        comp = c;
+        build_heap();
+    }
+
+    ~Heap()
+    {
+        delete[] arr;
+    }
+
+    void set_compare(bool (*c)(const T&, const T&))
+    {
+        comp = c;
+    }
+
+    void insert(T elem)
+    {
+        if (fool())
+        {
+            capacity *= INCREASE_FACTOR;
+
+            T* new_arr = new T[capacity];
+            for (int i = 0; i < size; ++i)
+                new_arr[i] = arr[i];
+
+            delete[] arr;
+            arr = new_arr;
+        }
+
+        arr[size] = elem;
+        sift_up(size);
+        size++;
+    }
+
+    T extract()
+    {
+        if (empty())
+            return NULL;
+
+        T result = arr[0];
+        arr[0] = arr[size - 1];
+
+        if (!empty())
+            sift_down(0);
+
+        size--;
+        return result;
+    }
+
+    void print()
+    {
+        if (!empty())
+        {
+            std::cout << arr[0] << std::endl;
+            
+            int left = 1;
+            int right = 2;
+            while (left < size)
+            {
+                for (int k = left; k <= right && k < size; ++k)
+                    std::cout << arr[k] << ' ';
+                left = left * 2 + 1;
+                right = right * 2 + 2;
+                std::cout << std::endl;
+            }
+        }
+        std::cout << std::endl;
+    }
+
+private:
+    void build_heap()
+    {
+        for(int i = (size - 1) / 2; i >= 0; --i)
+            sift_down(i);
+    }
+
+    bool fool()
+    {
+        return size == capacity;
+    }
+
+    bool empty()
+    {
+        return size == 0;
+    }
+
+    void sift_down(int i)
+    {
+        while (true)
+        {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            int largest = i;
+            if (left < size && comp(arr[left], arr[i]))
+                largest = left;
+            if (right < size && comp(arr[right], arr[largest]))
+                largest = right;
+
+            if (largest == i)
+                return;
+
+            std::swap(arr[i], arr[largest]);
+            i = largest;
+        }
+    }
+
+    void sift_up(int i)
+    {
+        while (i > 0)
+        {
+            int parent = (i - 1) / 2;
+            if (comp(arr[parent], arr[i]))
+                return;
+
+            std::swap(arr[i], arr[parent]);
+            i = parent;
+        }
+    }
+
+    T* arr;
+    int size, capacity;
+    bool (*comp)(const T&, const T&);
+};
+
+template <typename T>
+bool compare(const T& l, const T& r)
+{
+    return l > r;
+}
+
+
+
+int main()
+{
+    Heap<int> h;
+    h.set_compare(compare);
+
+    for (int i = 0; i < 100; ++i)
+        h.insert(i);
+
+    for (int i = 0; i < 100; ++i)
+        std::cout << h.extract() << '\n';
+
+    return 0;
+}
